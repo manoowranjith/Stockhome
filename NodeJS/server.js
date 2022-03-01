120665334586d658dc0e8fbeb97fd498a6529e41
MANOOWRANJITH.A.J <64479400+manoowranjith@users.noreply.github.com>
2022-03-01 12:37:54 +0530

    sonarcloud-bug-fix-1

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -19,7 +19,8 @@ app.use(bodyParser.urlencoded({extended: true}) )
 
 app.get('/search/:id',(req,res)=>{
   const url='https://www.pexels.com/api/v3/search/suggestions/'
-  axios(url+req.params.id)
+  var id=req.params.id
+  axios(url+id)
   .then(function (response) {
     res.json(response.data.data.attributes);
   }).catch(function (error) {
